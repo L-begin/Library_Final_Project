e7734719edcb92bd9df84591602c763f2458ffac
ASUS <765957247@qq.com>
2024-12-19 11:50:30 +0800

    test

---
+++ src/test/java/cn/lanqiao/library_final_project/LibraryFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lanqiao.library_final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
